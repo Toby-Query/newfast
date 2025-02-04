6bb390cbcb9f622c10415ebd4eaa711272e483c7
Toby-Query <tobyquery1@gmail.com>
2025-02-04 16:10:48 +0200

    adding tailwind

--- src/_data/characters.js
+++ src/_data/characters.js
@@ -1,10 +1,8 @@
-const axios = require("axios");
+// const axios = require("axios");
 
 module.exports = async function () {
   console.log("fetching characters");
-  const res = await axios.get("https://rickandmortyapi.com/api/character");
+  const res = await fetch("https://rickandmortyapi.com/api/character");
   const data = await res.json();
-  return {
-    results: "one",
-  };
+  return data;
 };
